4900b9af4f6e6e4d972d527ea7b93167e497e7fd
icegriffinguru <icegriffin1122@gmail.com>
2022-08-24 04:35:52 -0800

    nft minting complete on testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,7 @@
 /** @type import('hardhat/config').HardhatUserConfig */
+
+require('@nomiclabs/hardhat-waffle');
+
 module.exports = {
   solidity: "0.8.9",
 };
@@ -12,7 +15,14 @@ module.exports = {
    solidity: "0.8.9",
    defaultNetwork: "goerli",
    networks: {
-      hardhat: {},
+      hardhat: {
+         
+      },
+      rinkeby : {
+         url : "https://rinkeby.infura.io/v3/8ba2261b0b484ac6998be9ed9438eade",
+         chainId : 4,
+         accounts : [`0x${PRIVATE_KEY}`]
+      },
       goerli: {
          url: API_URL,
          accounts: [`0x${PRIVATE_KEY}`]
